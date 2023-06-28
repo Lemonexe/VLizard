50eedd9d22e74f82b059022ac3d5afb495f46b8a
Jiri Zbytovsky <zbytek@gmail.com>
2023-06-28 09:49:50 +0200

    App UI (#20)
    * use electron-vite-react template
    
    * tweak prettier & eslint setup
    
    * customize, format CRLF
    
    * use Mui, install reactQuery, axios

---
+++ appUI/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { Box, Button, ThemeProvider } from '@mui/material';
+
+// empty MUI theme object
+const theme = {};
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <ThemeProvider theme={theme}>
+            Henlo vlizerd!
+            <Box>
+                <Button variant="contained" color="primary" onClick={() => setCount((prev) => prev + 1)}>
+                    Click me
+                </Button>
+                <Box sx={{ m: 1 }}>{count}</Box>
+            </Box>
+        </ThemeProvider>
+    );
+}
+
+export default App;
