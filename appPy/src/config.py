e3a0838862fbe61f5479fa1ef1dad119189c2130
Jiri Zbytovsky <zbytek@gmail.com>
2023-08-02 15:31:39 +0200

    extract overridable config to json

--- appPy/src/config.py
+++ appPy/src/config.py
@@ -1,13 +1,32 @@
-# UI
+import json
+
+### OVERRIDABLE CONFIG
+
+with open('src/default_config.json', mode='r', encoding='utf-8') as file:
+    data = json.load(file)
+
+## Calculations: arbitrary tolerances & criterions
+
+# gamma tolerance for simple gamma test
+gamma_abs_tol = data['gamma_abs_tol']
+# when extrapolating Antoine, allow temperature to be out bounds by = this * (Antoine temp interval)
+antoine_bounds_rel_tol = data['antoine_bounds_rel_tol']
+# [%] in Redlich-Kister test, data is inconsistent if D > this
+rk_D_criterion = data['rk_D_criterion']
+# [%] in Herington test, data is inconsistent if |D-J| > this
+herington_DJ_criterion = data['herington_DJ_criterion']
+# relative tolerance for integration error in Redlich-Kister
+rk_quad_rel_tol = data['rk_quad_rel_tol']
+# [%] in Fredenslund test, when average residuals are above this threshold, data is declared inconsistent
+fredenslund_criterion = data['fredenslund_criterion']
+
+### IMMUTABLE CONFIG
+
+## CONSTANTS
+R = 8.31446  # [J/K/mol]
+
+## UI
 cli_fg_ok = 'green'
 cli_fg_err = 'bright_red'
 cli_fg_warn = 'yellow'
 x_points_smooth_plot = 100  # how many x points shall be tabelated when function is plotted
-
-# Calculations: arbitrary tolerances & criterions
-gamma_abs_tol = 3e-2  # gamma tolerance for simple gamma test
-antoine_bounds_rel_tol = 0.10  # when extrapolating Antoine, allow temperature to be out bounds by = this * (Antoine temp interval)
-rk_D_criterion = 10  # [%] in Redlich-Kister test, data is inconsistent if D > this
-herington_DJ_criterion = 10  # [%] in Herington test, data is inconsistent if |D-J| > this
-rk_quad_rel_tol = 1e-4  # relative tolerance for integration error in Redlich-Kister
-fredenslund_criterion = 1  # [%] in Fredenslund test, when average residuals are above this threshold, data is declared inconsistent
