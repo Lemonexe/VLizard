1a9d66282ce4f086dbd9e1243cfcc1cba8ce9e63
Jiri Zbytovsky <zbytek@gmail.com>
2023-08-11 13:21:57 +0200

    more sanitization on vector.py

--- appPy/src/utils/test_vector.py
+++ appPy/src/utils/test_vector.py
@@ -18,6 +18,11 @@ def test_pick_vector():
     assert np.array_equal(picked, [8, 6])
     assert np.array_equal(rest, [9, 7, 5])
 
+    # it doesn't make sense to duplicate indices, but it is acceptable, they'll be deduplicated
+    picked2, rest2 = pick_vector([9, 8, 7, 6, 5], [1, 1, 1, 1, 1, 1, 3, 3])
+    assert np.array_equal(picked2, picked)
+    assert np.array_equal(rest2, rest)
+
     picked, rest = pick_vector(all_params, idxs)
     assert np.array_equal(picked, vec_1)
     assert np.array_equal(rest, vec_2)
@@ -30,6 +35,12 @@ def test_pick_vector():
     # there must not be more indices than elements in original vector
     with pytest.raises(ValueError):
         pick_vector([1, 2], all_params)
+
+    # indices must be in bounds of vector
+    with pytest.raises(ValueError):
+        pick_vector([7, 7, 7], [99])
+    with pytest.raises(ValueError):
+        pick_vector([7, 7, 7], [-1])
 
 
 def test_overlay_vector():
@@ -45,3 +56,9 @@ def test_overlay_vector():
     # size of indices must match the first vector
     with pytest.raises(ValueError):
         overlay_vectors([7, 7, 7], [2, 3], vec_2)
+
+    # indices must be within bounds of newly created vector
+    with pytest.raises(ValueError):
+        overlay_vectors([7, 7], [1, 8], vec_2)
+    with pytest.raises(ValueError):
+        overlay_vectors([7], [-1], vec_2)
--- appPy/src/utils/vector.py
+++ appPy/src/utils/vector.py
@@ -4,11 +4,13 @@ import numpy as np
 # pick part of vector by list of indices, and the rest
 def pick_vector(vec, idxs):
     vec = np.array(vec)
-    idxs = np.array(idxs)
+    idxs = np.unique(idxs)
 
     if len(idxs) > len(vec):
         raise ValueError(f'length of idxs ({len(idxs)}) must be less than length of vec ({len(vec)})')
     if len(idxs) == 0: return np.array([]), vec
+    if np.max(idxs) >= len(vec) or np.min(idxs) < 0:
+        raise ValueError(f'index {np.max(idxs)} out of bounds, max {len(vec)-1}')
 
     mask = np.zeros(len(vec), dtype=bool)
     mask[idxs] = True
@@ -20,14 +22,18 @@ def pick_vector(vec, idxs):
 # merge two vectors given a list of indices of first vector in the whole, while the other vector fills the remaining space
 def overlay_vectors(vec_1, idxs_1, vec_2):
     vec_1 = np.array(vec_1)
-    idxs_1 = np.array(idxs_1)
+    idxs_1 = np.unique(idxs_1)
     vec_2 = np.array(vec_2)
+
+    n_new = len(vec_1) + len(vec_2)
 
     if len(vec_1) != len(idxs_1):
         raise ValueError(f'length of vec_1 ({len(vec_1)}) must equal length of idxs_1 ({len(vec_1)})')
     if len(vec_1) == 0: return vec_2
+    if np.max(idxs_1) >= n_new or np.min(idxs_1) < 0:
+        raise ValueError(f'index {np.max(idxs_1)} out of bounds, max {n_new-1}')
 
-    vec = np.zeros(len(vec_1) + len(vec_2))
+    vec = np.zeros(n_new)
     mask_1 = np.zeros(len(vec), dtype=bool)
     mask_1[idxs_1] = True
     vec[mask_1] = vec_1
