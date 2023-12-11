0017368860570047e6d9d3fba58771f653717344
Jiri Zbytovsky <zbytek@gmail.com>
2023-12-11 22:38:13 +0100

    use Test model for compound spec

--- appPy/src/utils/compounds_spec.py
+++ appPy/src/utils/compounds_spec.py
@@ -1,19 +1,27 @@
 import pytest
 import numpy as np
-from src.TD.vapor_models.antoine import antoine_model
+from src.TD.vapor_models.Vapor_Model import Vapor_Model
 from .compounds import get_vapor_model_params
 from .errors import AppException
 
-# NOTE: this test relies on the existence test parameters in data/ps/Antoine.tsv
+# NOTE: this test relies on the file data/ps/Test.tsv
 compound = 'test'
 invalid_compound = 'error'
 
+test_model = Vapor_Model(
+    name='Test',
+    n_params=3,
+    param_names=['A', 'B', 'C'],
+    fun=None,
+    params0=None,
+)
+
 
 def test_get_vapor_model_params():
-    T_min, T_max, params = get_vapor_model_params(compound, antoine_model)
+    T_min, T_max, params = get_vapor_model_params(compound, test_model)
     assert T_min == 100
     assert T_max == 200
     assert np.allclose(params, [1, 2, 3])
 
     with pytest.raises(AppException):
-        get_vapor_model_params(invalid_compound, antoine_model)
+        get_vapor_model_params(invalid_compound, test_model)
