3945b7b10189de7cb08803f718405cd93b539dff
Jiri Zbytovsky <zbytek@gmail.com>
2024-01-06 18:04:54 +0100

    header menu: add pure, binary, remove data, analysis

--- appUI/src/pages/Compounds/Compounds.tsx
+++ appUI/src/pages/Compounds/Compounds.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react';
+import { Button, Stack } from '@mui/material';
+import { PostAdd } from '@mui/icons-material';
+import { DefaultLayout } from '../../components/DefaultLayout.tsx';
 
 export const Compounds: FC = () => {
     return (
-        <>
-            <h3>Compounds</h3>
-        </>
+        <DefaultLayout>
+            <Stack direction="row" justifyContent="space-between">
+                <h2>Pure compounds data</h2>
+
+                <Button variant="contained" startIcon={<PostAdd />}>
+                    Add new
+                </Button>
+            </Stack>
+        </DefaultLayout>
     );
 };
--- appUI/src/pages/Systems/Systems.tsx
+++ appUI/src/pages/Systems/Systems.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react';
+import { Button, Stack } from '@mui/material';
+import { PostAdd } from '@mui/icons-material';
+import { DefaultLayout } from '../../components/DefaultLayout.tsx';
 
 export const Systems: FC = () => {
     return (
-        <>
-            <h3>Binary systems</h3>
-        </>
+        <DefaultLayout>
+            <Stack direction="row" justifyContent="space-between">
+                <h2>Binary systems data</h2>
+
+                <Button variant="contained" startIcon={<PostAdd />}>
+                    Add new
+                </Button>
+            </Stack>
+        </DefaultLayout>
     );
 };
