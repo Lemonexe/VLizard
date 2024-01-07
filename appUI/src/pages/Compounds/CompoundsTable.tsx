807551dd21ed62436ecd870323a51e24688e7eb8
Jiri Zbytovsky <zbytek@gmail.com>
2024-01-07 22:24:34 +0100

    move edit vapor to actions column

--- appUI/src/pages/Compounds/CompoundsTable.tsx
+++ appUI/src/pages/Compounds/CompoundsTable.tsx
@@ -10,15 +10,13 @@ type CompoundRowProps = { model: VaporModel };
 const CompoundRow: FC<CompoundRowProps> = ({ model: { compound, model_name } }) => (
     <TableRow sx={{ '&:last-child td': { border: 0 } }}>
         <TableCell>{compound}</TableCell>
-        <TableCell>
-            {model_name}
-            <Tooltip title="Change or edit vapor pressure model">
-                <IconButton children={<Edit />} />
-            </Tooltip>
-        </TableCell>
+        <TableCell>{model_name}</TableCell>
         <TableCell>
             <Tooltip title="Perform analysis">
                 <IconButton children={<QueryStats />} />
+            </Tooltip>
+            <Tooltip title="Change or edit vapor pressure model">
+                <IconButton children={<Edit />} />
             </Tooltip>
             <DeleteCompoundButton compound={compound} model_name={model_name} />
         </TableCell>
