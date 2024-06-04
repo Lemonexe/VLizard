abe28c22ca641415b3d3981d643c91bfc9062f7d
Jiri Zbytovsky <zbytek@gmail.com>
2024-06-04 22:34:51 +0200

    reapply child process
    This reverts commit 2724ef3633791847337d1bb320577d0e9c865610.

--- appUI/electron/main.ts
+++ appUI/electron/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, shell } from 'electron';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import path from 'node:path';
 
 // The built directory structure
@@ -14,6 +15,7 @@ process.env.DIST = path.join(__dirname, '../dist');
 process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
 
 let win: BrowserWindow | null;
+let child: ChildProcessWithoutNullStreams | undefined;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
 
@@ -52,8 +54,25 @@ function createWindow() {
     }
 }
 
+// Start python backend server
+function startServer() {
+    // only for the installed app, not for development
+    if (!app.isPackaged) return;
+
+    // const appPyPath = path.join(__dirname, '..', '..', 'appPy');
+    // child = spawn('pipenv', ['run', 'start'], { cwd: appPyPath, shell: true });
+
+    const exePath = path.join(process.resourcesPath, 'serve.exe');
+    child = spawn(exePath, [], { cwd: process.resourcesPath });
+}
+
+app.on('quit', () => child?.kill());
+
 app.on('window-all-closed', () => {
     win = null;
 });
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    startServer();
+    createWindow();
+});
