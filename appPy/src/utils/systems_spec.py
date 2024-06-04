f822b629e57dc81020516623a50b9338b2e92244
Jiri Zbytovsky <zbytek@gmail.com>
2024-06-04 14:49:18 +0200

    pytest: mock to use local test files

--- appPy/src/utils/systems_spec.py
+++ appPy/src/utils/systems_spec.py
@@ -1,14 +1,17 @@
 import pytest
-import os
+from .io.local_files import join_data_path
 from .systems import validate_system_or_swap, get_system_path
 from .errors import AppException
 
-# NOTE: this test relies on the existence of the test system in data/VLE
+# NOTE: this test relies on the files in appPy\data\VLE\test1-test2
 compound1 = 'test1'
 compound2 = 'test2'
 
 
-def test_validate_system_or_swap():
+def test_validate_system_or_swap(mocker):
+    # use testing data instead of real userdata files
+    mocker.patch('src.utils.io.local_files.data_folder_path', 'data')
+
     # correct order
     assert validate_system_or_swap(compound1=compound1, compound2=compound2) == (compound1, compound2)
 
@@ -19,6 +22,7 @@ def test_validate_system_or_swap():
         validate_system_or_swap('nonsense', 'nonsense')
 
 
+# pure function, no file system side effects
 def test_get_system_path():
-    file_path = os.path.join('data', 'VLE', 'benzene-toluene')
+    file_path = join_data_path('VLE', 'benzene-toluene')
     assert get_system_path('benzene', 'toluene') == file_path
