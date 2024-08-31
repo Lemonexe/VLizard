8a844a19e171884c19f5d82116b85bcb2e2a8a71
Jiri Zbytovsky <zbytek@gmail.com>
2024-08-31 21:44:43 +0200

    v2024.1.0, use year instead of major SemVer

--- appUI/src/pages/About.tsx
+++ appUI/src/pages/About.tsx
@@ -5,9 +5,12 @@ import { DefaultLayout } from '../components/DefaultLayout.tsx';
 
 const currentYear = new Date().getFullYear();
 
+// display only major and minor version, not patch
+const DisplayedAppVersion = APP_VERSION.split('.').slice(0, 2).join('.');
+
 export const About: FC = () => (
     <DefaultLayout>
-        <h1>VLizard v{APP_VERSION}</h1>
+        <h1>VLizard v{DisplayedAppVersion}</h1>
         <Box my={2}>
             <Button
                 startIcon={<QuestionMark />}
@@ -17,9 +20,9 @@ export const About: FC = () => (
                 User manual
             </Button>
         </Box>
+        <p>Created by Jiří Zbytovský in 2023–{currentYear}.</p>
         <p>
-            Created by Jiří Zbytovský in 2023–{currentYear}, hosted at{' '}
-            <a href="https://github.com/Lemonexe/VLizard/">Github repository</a> under the free{' '}
+            Hosted at <a href="https://github.com/Lemonexe/VLizard/">Github repository</a> under the free{' '}
             <a href="https://github.com/Lemonexe/VLizard/blob/master/LICENSE">MIT License</a>.
         </p>
     </DefaultLayout>
