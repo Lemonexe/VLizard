1cc1aefa05dccf9c3ce1a138d75211457f612274
Jiri Zbytovsky <zbytek@gmail.com>
2025-01-26 22:40:38 +0100

    DONT MERGE; 300 DPI, legend inside images

--- appPy/src/plot/Gamma_plot.py
+++ appPy/src/plot/Gamma_plot.py
@@ -14,7 +14,6 @@ class Gamma_plot(Gamma_test, VLE_plot):
         plt.plot(self.x_tab, self.gamma_tab_1, '-r', label='$\\gamma_1$ van Laar')
         plt.plot(self.x_tab, self.gamma_tab_2, '-b', label='$\\gamma_2$ van Laar')
 
-        plt.subplots_adjust(left=0.1, right=0.7)  # Reduce the right margin and make the plot lean to the left
-        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legend outside center right
+        plt.legend(loc='upper center')
 
         return finish_plot(mode)
