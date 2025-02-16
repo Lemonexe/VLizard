59bea53609dcc5086ab20a52406a27c2fbdedd86
Jiri Zbytovsky <zbytek@gmail.com>
2025-02-16 23:10:21 +0100

    DONT MERGE; iecred TOC graphics

--- appPy/src/plot/Gamma_plot.py
+++ appPy/src/plot/Gamma_plot.py
@@ -11,9 +11,12 @@ class Gamma_plot(Gamma_test, VLE_plot):
 
         self.plot_gamma(mode=None)
 
-        plt.plot(self.x_tab, self.gamma_tab_1, '-r', label='$\\gamma_1$ van Laar')
-        plt.plot(self.x_tab, self.gamma_tab_2, '-b', label='$\\gamma_2$ van Laar')
+        plt.plot(self.x_tab, self.gamma_tab_1, '-r', label='$\\gamma_1$ model')
+        plt.plot(self.x_tab, self.gamma_tab_2, '-g', label='$\\gamma_2$ model')
 
-        plt.legend(loc='upper center')
+        plt.subplots_adjust(left=0.1, right=0.7)  # Reduce the right margin and make the plot lean to the left
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legend outside center right
+
+        plt.axis([0, 1, 0.8, 2])
 
         return finish_plot(mode)
