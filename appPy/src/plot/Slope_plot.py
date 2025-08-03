05386d34a06dc56a40f6c1940528ebf64be06f97
Jiri Zbytovsky <zbytek@gmail.com>
2025-08-03 23:17:38 +0200

    Slope plot tweak

--- appPy/src/plot/Slope_plot.py
+++ appPy/src/plot/Slope_plot.py
@@ -15,7 +15,7 @@ class Slope_plot(Slope_test):
         plt.xlim(0, 1)
         plt.xticks(np.linspace(0, 1, 11))
         plt.xlabel('$x_1$')
-        plt.ylabel('$d$ln$\\gamma$ resid')
+        plt.ylabel('residual')
         x_dynamic_number_format()
 
         return finish_plot(mode)
@@ -23,14 +23,14 @@ class Slope_plot(Slope_test):
     def plot_derivations(self, mode):
         init_plot(mode)
 
-        plt.plot(self.x_1, self.d_ln_gamma_1, 'Dr', label='$d$ln$\\gamma_1$')
-        plt.plot(self.x_1, self.d_ln_gamma_2, 'vb', label='$d$ln$\\gamma_2$')
+        plt.plot(self.x_1, self.d_ln_gamma_1, 'Dr', label='d ln$\\gamma_1$ / d $x_1$')
+        plt.plot(self.x_1, self.d_ln_gamma_2, 'vb', label='d ln$\\gamma_2$ / d $x_1$')
         plt.axhline(y=0, color='k', linestyle=':')
         plt.title(self.get_title())
         plt.xlim(0, 1)
         plt.xticks(np.linspace(0, 1, 11))
         plt.xlabel('$x_1$')
-        plt.ylabel('$d$ln$\\gamma$')
+        plt.ylabel('d ln$\\gamma$ / d $x_1$')
         plt.legend()
         x_dynamic_number_format()
 
